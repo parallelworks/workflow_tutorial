d9dbf2256fcf0a4575573cba90b0f29145be351a
Alvaro Vidal Torreira <avidalto@hawk.iit.edu>
2023-07-06 12:23:17 -0500

    Update main.sh

--- 001_single_resource_command/main.sh
+++ 001_single_resource_command/main.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# Load  workflow parameter names and values from the XML as environment variables
+# In this case: command, resource_1_username, resource_1_publicIp
 source inputs.sh
 
 echo; echo "Running command [${command}] on resource 1"
