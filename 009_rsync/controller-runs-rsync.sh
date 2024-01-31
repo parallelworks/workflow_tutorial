e7e49fec1e77421878f0cd1a8b788cd6fdfa5144
avidalto <avidalto@hawk.iit.edu>
2024-01-31 05:01:54 -0600

    Support ssh config not included in ~/.ssh/config

--- 009_rsync/controller-runs-rsync.sh
+++ 009_rsync/controller-runs-rsync.sh
@@ -8,6 +8,17 @@ CLUSTER_JOB_DIR=__CLUSTER_JOB_DIR__
 mkdir -p ${CLUSTER_JOB_DIR}
 cd ${CLUSTER_JOB_DIR}
 
+# Get SSH options to ssh back to the user container
+# In On-Prem clusters the SSH config is not included
+# in ~/.ssh/config. 
+if [ -e "${HOME}/pw/.pw/config" ]; then
+    # Check if the SSH config for the compute node exists
+    # Assumes the work directory of the cluster is ${HOME}
+    SSH_USERCONTAINER_OPTIONS="-F ${HOME}/pw/.pw/config"
+else
+    SSH_USERCONTAINER_OPTIONS=""
+fi
+
 #######################################
 # CONTROLLER NODE INITIATES TRANSFERS #
 #######################################
@@ -21,7 +32,7 @@ origin="usercontainer:${UCONTAINER_JOB_DIR}/transfer-directory/"
 destination="controller-transfer-to-controller-from-usercontainer/"
 
 echo; echo "Transferring directory from ${origin} to ${destination}"
-rsync -avzq --rsync-path="mkdir -p ${UCONTAINER_JOB_DIR} && rsync" ${origin} ${destination}
+rsync -avzq -e "ssh ${SSH_USERCONTAINER_OPTIONS}" --rsync-path="mkdir -p ${UCONTAINER_JOB_DIR} && rsync" ${origin} ${destination}
 
 echo "Hello from the controller node $(hostname)" > ${destination}/greetings.txt
 
@@ -32,4 +43,4 @@ origin="controller-transfer-to-controller-from-usercontainer/"
 destination="usercontainer:${UCONTAINER_JOB_DIR}/controller-transfer-to-usercontainer-from-controller/"
 
 echo; echo "Transferring directory from ${origin} to ${destination}"
-rsync -avzq ${origin} ${destination}
+rsync -avzq -e "ssh ${SSH_USERCONTAINER_OPTIONS}" ${origin} ${destination}
--- 012_bucket_transfer/load_bucket_credentials.sh
+++ 012_bucket_transfer/load_bucket_credentials.sh
@@ -4,4 +4,17 @@ echo "LOADING BUCKET NAME AND SHORT-TERM CREDENTIALS"
 # Load bucket name and credentials by calling the bucket_token_generator.py script
 # This script needs to be in your user container because it needs access to the PW_API_KEY environment variable
 # If this script runs in the controller node the jump host in the ssh command is optional 
-eval $(ssh -J ${resource_privateIp} usercontainer ${token_generator_path} --bucket_id ${bucket_id} --token_format text)
+
+# Get SSH options to ssh back to the user container
+# In On-Prem clusters the SSH config is not included
+# in ~/.ssh/config. 
+if [ -e "${HOME}/pw/.pw/config" ]; then
+    # Check if the SSH config for the compute node exists
+    # Assumes the work directory of the cluster is ${HOME}
+    SSH_USERCONTAINER_OPTIONS="-F ${HOME}/pw/.pw/config"
+else
+    SSH_USERCONTAINER_OPTIONS="-J ${resource_privateIp}"
+fi
+
+
+eval $(ssh ${SSH_USERCONTAINER_OPTIONS} usercontainer ${token_generator_path} --bucket_id ${bucket_id} --token_format text)
