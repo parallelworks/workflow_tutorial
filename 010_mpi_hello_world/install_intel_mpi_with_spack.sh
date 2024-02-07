186eb1cd23b6b8ff932d07d36b91d0e916b1ffc2
avidalto <avidalto@hawk.iit.edu>
2024-02-07 06:16:19 -0600

    Install intel mpi with spack by default

--- 010_mpi_hello_world/install_intel_mpi_with_spack.sh
+++ 010_mpi_hello_world/install_intel_mpi_with_spack.sh
@@ -12,7 +12,7 @@ mkdir -p pw
 cd pw
 git clone ${code_repo}
 git clone -c feature.manyFiles=true https://github.com/spack/spack.git
-. $HOME/spack/share/spack/setup-env.sh
+. $HOME/pw/spack/share/spack/setup-env.sh
 spack install intel-oneapi-mpi intel-oneapi-compilers gcc-runtime
 source /usr/share/lmod/8.7.7/init/bash
 yes | spack module lmod refresh gcc-runtime intel-oneapi-mpi intel-oneapi-compilers
@@ -20,9 +20,9 @@ yes | spack module lmod refresh gcc-runtime intel-oneapi-mpi intel-oneapi-compil
 # Create file to load the environment
 cat > load-intel-oneapi-mpi.sh <<HERE
 #!/bin/bash
-. $HOME/spack/share/spack/setup-env.sh
+. $HOME/pw/spack/share/spack/setup-env.sh
 source /usr/share/lmod/8.7.7/init/bash
-export MODULEPATH=$MODULEPATH:$HOME/spack/share/spack/lmod/linux-centos7-x86_64/Core
+export MODULEPATH=$MODULEPATH:$HOME/pw/spack/share/spack/lmod/linux-centos7-x86_64/Core
 module load intel-oneapi-mpi/2021.11.0-c7j5bm5
 module load intel-oneapi-compilers/2024.0.2-z5h22vh
 HERE
