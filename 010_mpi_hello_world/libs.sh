908702f5832799afb6e60cac586f1e5906f8a0a6
avidalto <avidalto@hawk.iit.edu>
2024-02-07 06:34:54 -0600

    Install intel mpi with spack by default

--- 010_mpi_hello_world/libs.sh
+++ 010_mpi_hello_world/libs.sh
@@ -1,34 +1,52 @@
 #!/bin/bash
 
+
+
+get_slurm_job_status() {
+    # Get the header line to determine the column index corresponding to the job status
+    if [ -z "${SQUEUE_HEADER}" ]; then
+        export SQUEUE_HEADER="$(eval "$sshcmd ${status_cmd}" | awk 'NR==1')"
+    fi
+    status_column=$(echo "${SQUEUE_HEADER}" | awk '{ for (i=1; i<=NF; i++) if ($i ~ /^S/) { print i; exit } }')
+    status_response=$(eval $sshcmd ${status_cmd} | grep "\<${jobid}\>")
+    echo "${SQUEUE_HEADER}"
+    echo "${status_response}"
+    export job_status=$(echo ${status_response} | awk -v id="${jobid}" -v col="$status_column" '{print $col}')
+}
+
+get_pbs_job_status() {
+    # Get the header line to determine the column index corresponding to the job status
+    if [ -z "${QSTAT_HEADER}" ]; then
+        export QSTAT_HEADER="$(eval "$sshcmd ${status_cmd}" | awk 'NR==1')"
+    fi
+    status_response=$(eval $sshcmd ${status_cmd} 2>/dev/null | grep "\<${jobid}\>")
+    echo "${QSTAT_HEADER}"
+    echo "${status_response}"
+    export job_status="$(eval $sshcmd ${status_cmd} -f ${jobid} 2>/dev/null  | grep job_state | cut -d'=' -f2 | tr -d ' ')"
+
+}
+
+
 # FIXME: Support failure as well?
 wait_job() {
     while true; do
+        sleep 15
         # squeue won't give you status of jobs that are not running or waiting to run
         # qstat returns the status of all recent jobs
-        job_status=$($sshcmd ${status_cmd} | grep ${jobid} | awk '{print $5}')
         if [[ ${jobschedulertype} == "SLURM" ]]; then
+            get_slurm_job_status
             # If job status is empty job is no longer running
             if [ -z "${job_status}" ]; then
                 job_status=$($sshcmd sacct -j ${jobid}  --format=state | tail -n1)
-                echo "    Job ${jobid} exited with status ${job_status}"
-                if [[ "${job_status}" == *"FAILED"* ]]; then
-                    echo "ERROR: SLURM job [${slurm_job}] failed"
-                    exit 1
-                else
-                    break
-                fi
-            fi
-        elif [[ ${jobschedulertype} == "PBS" ]]; then
-            if [[ "${job_status}" == "C" ]]; then
-                echo "Job ${jobid} exited with status C"
                 break
             fi
-            if [ -z "${job_status}" ]; then
-                echo "Job ${jobid} exited"
+        elif [[ ${jobschedulertype} == "PBS" ]]; then
+            get_pbs_job_status
+            if [[ "${job_status}" == "C" ]]; then
+                break
+            elif [ -z "${job_status}" ]; then
                 break
             fi
         fi
-        echo "    Job ${jobid} status: ${job_status}"
-        sleep 5
     done
 }
