e05fd32a7b8d287b198fc6a7fe9874f82d6bfce4
avidalto <avidalto@hawk.iit.edu>
2024-02-20 08:02:21 -0600

    Added mpi docker hello world

---
+++ 013_multihost_docker_mpi/README.md
@@ -0,0 +1,4 @@
+## MPI Docker Hello World
+This workflow demonstrates executing a multinode MPI job within a SLURM cluster using Docker. It comprises two scripts: main.sh and hello-world-mpi-docker.sh. main.sh submits hello-world-mpi-docker.sh to the chosen SLURM resource through SSH. hello-world-mpi-docker.sh is an independent script illustrating the utilization of multinode MPI with Docker in a SLURM environment, employing Docker Swarm to establish a container cluster.
+
+Users can specify a Docker repository containing OpenMPI and configure the number of nodes and partition scheduler directives for job submission. Notably, all available cores are utilized for the MPI hello world, thus --exclusive scheduler directive is hardcoded in hello-world-mpi-docker.sh. Additionally, --ntasks-per-node is set to 1 in hello-world-mpi-docker.sh, reflecting a single Docker container per node.
---
+++ 013_multihost_docker_mpi/main.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# This wrapper launches the hello-world-mpi-docker.sh script in the selected resource 
+APP_DIR=$(dirname $0)
+source ${APP_DIR}/libs.sh
+
+source inputs.sh
+
+sshcmd="ssh -o StrictHostKeyChecking=no ${resource_username}@${resource_publicIp}"
+echo "SSH command"
+echo ${sshcmd}
+
+# JOB_DIR=<workflow-name>/<job-number>
+JOB_DIR=$(pwd | rev | cut -d'/' -f1-2 | rev)
+# Absolute path to the job directory in the user space
+export UCONTAINER_JOB_DIR=${PWD}
+# Absolute path to the job directory in the remote resource
+export CLUSTER_JOB_DIR=${resource_workdir}/pw/${JOB_DIR}/
+
+echo; echo; echo CREATING SLURM SCRIPT
+sed -i "s/__OMPI_CONTAINER_REPO__/${ompi_container_repo}/g" hello-world-mpi-docker.sh
+sed -i "s/__NODES__/${nodes}/g" hello-world-mpi-docker.sh
+if [ -z "${partition}" ]; then
+    sed -i '/#SBATCH --partition=__PARTITION__/d' your_file_name
+else
+    sed -i "s/__PARTITION__/${partition}/g" hello-world-mpi-docker.sh
+fi
+
+echo; echo; echo TRANSFERRING NECESSARY FILES TO CLUSTER
+# Create remote directory
+${sshcmd} mkdir -p ${CLUSTER_JOB_DIR}
+scp ${APP_DIR}/hello-world-mpi-docker.sh ${resource_username}@${resource_publicIp}:${CLUSTER_JOB_DIR}
+
+echo; echo; echo SUBMITTING JOB
+export jobid=$(${sshcmd} sbatch ${CLUSTER_JOB_DIR}/hello-world-mpi-docker.sh | tail -1 | awk -F ' ' '{print $4}')
+
+# Write cancel script to remove/cancel the submitted job from the queue if the pw job is canceled
+echo "#!/bin/bash" > cancel.sh
+echo "${sshcmd} ${cancel_cmd} ${jobid}" >> cancel.sh
+chmod +x cancel.sh
+# Wait for submitted job to complete before exiting pw job
+wait_job
+# Make sure job is canceled before exiting the workflow
+./cancel.sh
+
+echo; echo; echo PRINTING OUTPUT
+${sshcmd} "cat ${CLUSTER_JOB_DIR}/mpi_hello_world.out"
+
