096131c68540644ca6139a86d09697fd00e107cc
avidalto <avidalto@hawk.iit.edu>
2024-02-20 08:23:39 -0600

    Added mpi docker hello world

--- 013_multihost_docker_mpi/main.sh
+++ 013_multihost_docker_mpi/main.sh
@@ -17,12 +17,12 @@ export UCONTAINER_JOB_DIR=${PWD}
 export CLUSTER_JOB_DIR=${resource_workdir}/pw/${JOB_DIR}/
 
 echo; echo; echo CREATING SLURM SCRIPT
-sed -i "s/__OMPI_CONTAINER_REPO__/${ompi_container_repo}/g" hello-world-mpi-docker.sh
-sed -i "s/__NODES__/${nodes}/g" hello-world-mpi-docker.sh
+sed -i "s/__OMPI_CONTAINER_REPO__/${ompi_container_repo}/g" ${APP_DIR}/hello-world-mpi-docker.sh
+sed -i "s/__NODES__/${nodes}/g" ${APP_DIR}/hello-world-mpi-docker.sh
 if [ -z "${partition}" ]; then
-    sed -i '/#SBATCH --partition=__PARTITION__/d' your_file_name
+    sed -i '/#SBATCH --partition=__PARTITION__/d' ${APP_DIR}/hello-world-mpi-docker.sh
 else
-    sed -i "s/__PARTITION__/${partition}/g" hello-world-mpi-docker.sh
+    sed -i "s/__PARTITION__/${partition}/g" ${APP_DIR}/hello-world-mpi-docker.sh
 fi
 
 echo; echo; echo TRANSFERRING NECESSARY FILES TO CLUSTER
