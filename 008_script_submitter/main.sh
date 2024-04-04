ea33a77288d0361d111a0fab78c96f0c4bf49090
avidalto <avidalto@hawk.iit.edu>
2024-04-04 07:48:16 -0500

    Check that slurmctl is running before submitting the job

--- 008_script_submitter/main.sh
+++ 008_script_submitter/main.sh
@@ -31,7 +31,7 @@ if [[ ${input_method} == "TEXT" ]] || [[ ${input_method} == "WORKSPACE_PATH" ]];
 fi
 
 # SUBMIT JOB
-echo; echo "Submitting job with command:"
+echo; echo "Submitting job"
 if [[ ${jobschedulertype} == "SLURM" ]]; then
     check_slurm
     submit_cmd="sbatch"
