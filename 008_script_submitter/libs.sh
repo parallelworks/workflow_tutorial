c755a2bd486d692ee0f424ec85e028fddf3a41c9
avidalto <avidalto@hawk.iit.edu>
2024-10-16 08:39:55 -0500

    Convert script submitter to v3

--- 008_script_submitter/libs.sh
+++ 008_script_submitter/libs.sh
@@ -1,57 +1,52 @@
 #!/bin/bash
 
+
+
+get_slurm_job_status() {
+    # Get the header line to determine the column index corresponding to the job status
+    if [ -z "${SQUEUE_HEADER}" ]; then
+        export SQUEUE_HEADER="$(eval "$sshcmd squeue" | awk 'NR==1')"
+    fi
+    status_column=$(echo "${SQUEUE_HEADER}" | awk '{ for (i=1; i<=NF; i++) if ($i ~ /^S/) { print i; exit } }')
+    status_response=$(eval $sshcmd squeue | grep "\<${jobid}\>")
+    echo "${SQUEUE_HEADER}"
+    echo "${status_response}"
+    export job_status=$(echo ${status_response} | awk -v id="${jobid}" -v col="$status_column" '{print $col}')
+}
+
+get_pbs_job_status() {
+    # Get the header line to determine the column index corresponding to the job status
+    if [ -z "${QSTAT_HEADER}" ]; then
+        export QSTAT_HEADER="$(eval "$sshcmd qstat" | awk 'NR==1')"
+    fi
+    status_response=$(eval $sshcmd qstat 2>/dev/null | grep "\<${jobid}\>")
+    echo "${QSTAT_HEADER}"
+    echo "${status_response}"
+    export job_status="$(eval $sshcmd qstat -f ${jobid} 2>/dev/null  | grep job_state | cut -d'=' -f2 | tr -d ' ')"
+
+}
+
+
 # FIXME: Support failure as well?
 wait_job() {
     while true; do
+        sleep 15
         # squeue won't give you status of jobs that are not running or waiting to run
         # qstat returns the status of all recent jobs
-        job_status=$($sshcmd ${status_cmd} | grep ${jobid} | awk '{print $5}')
         if [[ ${jobschedulertype} == "SLURM" ]]; then
+            get_slurm_job_status
             # If job status is empty job is no longer running
             if [ -z "${job_status}" ]; then
                 job_status=$($sshcmd sacct -j ${jobid}  --format=state | tail -n1)
-                echo "    Job ${jobid} exited with status ${job_status}"
-                if [[ "${job_status}" == *"FAILED"* ]]; then
-                    echo "ERROR: SLURM job [${slurm_job}] failed"
-                    exit 1
-                else
-                    break
-                fi
-            fi
-        elif [[ ${jobschedulertype} == "PBS" ]]; then
-            if [[ "${job_status}" == "C" ]]; then
-                echo "Job ${jobid} exited with status C"
                 break
             fi
-            if [ -z "${job_status}" ]; then
-                echo "Job ${jobid} exited"
+        elif [[ ${jobschedulertype} == "PBS" ]]; then
+            get_pbs_job_status
+            if [[ "${job_status}" == "C" ]]; then
+                break
+            elif [ -z "${job_status}" ]; then
                 break
             fi
         fi
-        echo "    Job ${jobid} status: ${job_status}"
-        sleep 15
-    done
-}
-
-check_slurm() {
-    # Run ps command to check if slurmctld is running and exclude the grep process
-    result=$(${sshcmd} ps aux | grep slurmctld | grep -v grep)
-
-    # Check if the result is empty
-    if [ -z "$result" ]; then
-        echo "ERROR: slurmctld is not running in the selected resource ${resource_name}"
-        echo "       Exiting workflow!"
-        exit 1
-    fi
-}
-
-# Function to print the SLURM logs
-print_slurm_logs() {
-    echo; echo; "SLURM LOGS"
-    local log_file_paths=$1
-    for log_file in ${log_file_paths}; do
-        echo "${sshcmd} cat ${log_file}"
-        ${sshcmd} cat ${log_file}
-        echo
     done
 }
